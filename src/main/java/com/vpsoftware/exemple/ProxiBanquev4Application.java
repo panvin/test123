71dce888d1a2fc030cb7d3dbbba19254110f21f7
Vincent PANOUILLERES <panouilleres.vincent@yahoo.fr>
2016-10-05 10:14:16 +0200

    Creation du projet git

---
+++ src/main/java/com/vpsoftware/exemple/ProxiBanquev4Application.java
@@ -0,0 +1,12 @@
+package com.vpsoftware.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanquev4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxiBanquev4Application.class, args);
+	}
+}
---
+++ src/test/java/com/vpsoftware/exemple/ProxiBanquev4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vpsoftware.exemple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProxiBanquev4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
