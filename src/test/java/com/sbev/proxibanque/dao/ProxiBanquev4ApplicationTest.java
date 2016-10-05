44907882e6a5a8718dcf9ca5f7c9f9639f5dbc4c
Vincent PANOUILLERES <panouilleres.vincent@yahoo.fr>
2016-10-05 17:01:43 +0200

    Modification des tests2

---
+++ src/test/java/com/sbev/proxibanque/dao/ProxiBanquev4ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.sbev.proxibanque.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProxiBanquev4ApplicationTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
